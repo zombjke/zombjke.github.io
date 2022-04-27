d05960ac70056553004a71973f0c2f95106899ce
ZombJkee <rasulov.lg@gmail.com>
2022-04-27 14:06:26 +0500

    initial

---
+++ javascripts/fragments.js
@@ -0,0 +1,416 @@
+const roadmap = 
+`<div id="backplace" class="backplace">
+    <div id="logo-roadmap"><span>roadmap</span></div>
+    <div id="backgroundpath" class="backgroundpath">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 637.96 235.69">
+            <linearGradient id="gradient">
+                <stop offset="0" stop-color="BlueViolet"></stop>
+                <stop offset="100%" stop-color="DarkSlateBlue"></stop>
+            </linearGradient>
+            <polyline  points="0 121.63 352.19 121.63 352.19 161.49 456.69 161.49 456.69 200.02 456.69 200.02 483.84 200.02"/>
+            <polyline  points="0 121.63 45.69 121.63 45.69 69.99 142.19 69.99 142.19 0.5 258.11 0.5"/>
+            <polyline  points="0 121.63 67.7 121.63 67.7 177.49 143.86 177.49 143.86 235.19 263.65 235.19"/>
+            <polyline  points="0 121.63 161.25 121.63 161.25 68.11 206.25 68.11"/>
+            <polyline  points="0 121.63 193.19 121.63 193.19 166.49 218.59 166.49"/>
+            <polyline  points="0 121.63 352.19 121.63 352.19 50.9 459.76 50.9"/>
+            <polyline  points="0 121.63 352.19 121.63 352.19 161.49 456.69 161.49 456.69 161.49"/>
+        </svg>
+    </div>
+    <div id="dotsvg-1" class="dotsvg">
+        <object type="image/svg+xml" data="/images/path/dott.svg"></object>
+    </div>
+    <div id="dotsvg-2" class="dotsvg">
+        <object type="image/svg+xml" data="/images/path/dott.svg"></object>
+    </div>
+    <div id="dotsvg-3" class="dotsvg">
+        <object type="image/svg+xml" data="/images/path/dott.svg"></object>
+    </div>
+    <div id="dotsvg-4" class="dotsvg">
+        <object type="image/svg+xml" data="/images/path/dott.svg"></object>
+    </div>
+    <div id="dotsvg-5" class="dotsvg">
+        <object type="image/svg+xml" data="/images/path/dott.svg"></object>
+    </div>
+    <div id="dotsvg-7" class="dotsvg">
+        <object type="image/svg+xml" data="/images/path/dott.svg"></object>
+    </div>
+    <div id="col-1" class="col">
+        <div id="html-box" class="smallBox">
+            <div id="htmltopbox" class="topBox">
+                <div id="htmlforsvg" class="svgBox">
+                    <object type="image/svg+xml" data="/images/icons/html-5.svg" width="34px" height="38px" ></object>
+                </div>
+                <div id="htmlnamesvg" class="namesvg">HTML5</div>
+            </div>
+            <div id="htmlprogressBar" class="bottomBox"></div>
+        </div>
+        
+        <div id="css-box" class="smallBox">
+            <div id="csstopbox" class="topBox">
+                <div id="cssforsvg" class="svgBox">
+                    <object type="image/svg+xml" data="/images/icons/css3.svg" width="34px" height="38px" ></object>
+                </div>
+                <div id="cssnamesvg" class="namesvg">CSS3</div>
+            </div>
+            <div id="cssprogressBar" class="bottomBox"></div>
+        </div>
+    </div>
+    <div id="col-2" class="col">
+        <div id="wordpress-box" class="smallBox">
+            <div id="wordpresstopbox" class="topBox">
+                <div id="wordpressforsvg" class="svgBox">
+                    <object type="image/svg+xml" data="/images/icons/wordpress.svg" width="34px" height="38px" ></object>
+                </div>
+                <div id="wordpressnamesvg" class="namesvg">Wordpress</div>
+             
+            </div>
+            <div id="wordpressprogressBar" class="bottomBox"></div>
+        </div>
+        
+        <div id="bootstrap-box" class="smallBox">
+            <div id="bootstraptopbox" class="topBox">
+                <div id="bootstrapforsvg" class="svgBox">
+                    <object type="image/svg+xml" data="/images/icons/bootstrap.svg" width="34px" height="38px" ></object>
+                </div>
+                <div id="bootstrapnamesvg" class="namesvg">Bootstrap</div>
+             
+            </div>
+            <div id="bootstrapprogressBar" class="bottomBox"></div>
+        </div>
+    </div>
+    <div id="col-3" class="col">
+        <div id="mysql-box" class="smallBox">
+            <div id="mysqltopbox" class="topBox">
+                <div id="mysqlforsvg" class="svgBox">
+                    <object type="image/svg+xml" data="/images/icons/mysql.svg" width="34px" height="38px" ></object>
+                </div>
+                <div id="mysqlnamesvg" class="namesvg">MySQL</div>
+             
+            </div>
+            <div id="mysqlprogressBar" class="bottomBox"></div>
+        </div>
+
+        <div id="git-box" class="smallBox">
+            <div id="gittopbox" class="topBox">
+                <div id="gitforsvg" class="svgBox">
+                    <object type="image/svg+xml" data="/images/icons/git.svg" width="34px" height="38px" ></object>
+                </div>
+                <div id="gitnamesvg" class="namesvg">Git</div>
+             
+            </div>
+            <div id="gitprogressBar" class="bottomBox"></div>
+        </div>
+    </div>
+    <div id="col-4" class="col">
+        <div id="woo-box" class="smallBox">
+            <div id="wootopbox" class="topBox">
+                <div id="wooforsvg" class="svgBox">
+                    <object type="image/svg+xml" data="/images/icons/woocommerce.svg" width="34px" height="38px" ></object>
+                </div>
+                <div id="woonamesvg" class="namesvg">Woo commerce</div>
+             
+            </div>
+            <div id="wooprogressBar" class="bottomBox"></div>
+        </div>
+
+        <div id="js-box" class="smallBox">
+            <div id="jstopbox" class="topBox">
+                <div id="jsforsvg" class="svgBox">
+                    <object type="image/svg+xml" data="/images/icons/javascript.svg" width="34px" height="38px" ></object>
+                </div>
+                <div id="jsnamesvg" class="namesvg">JavaScript</div>
+             
+            </div>
+            <div id="jsprogressBar" class="bottomBox"></div>
+        </div>
+
+        <div id="sass-box" class="smallBox">
+            <div id="sasstopbox" class="topBox">
+                <div id="sassforsvg" class="svgBox">
+                    <object type="image/svg+xml" data="/images/icons/LESS.svg" width="34px" height="38px" ></object>
+                </div>
+                <div id="sassnamesvg" class="namesvg">Less</div>
+             
+            </div>
+            <div id="sassprogressBar" class="bottomBox"></div>
+        </div>
+    </div>
+    <div id="col-5" class="col"></div>
+    <div id="col-6" class="col">
+        <div id="node-box" class="smallBox">
+            <div id="nodetopbox" class="topBox">
+                <div id="nodeforsvg" class="svgBox">
+                    <object type="image/svg+xml" data="/images/icons/nodejs.svg" width="34px" height="38px" ></object>
+                </div>
+                <div id="nodenamesvg" class="namesvg">Node.JS</div>
+             
+            </div>
+            <div id="nodeprogressBar" class="bottomBox"></div>
+        </div>
+
+        <div id="ts-box" class="smallBox">
+            <div id="tstopbox" class="topBox">
+                <div id="tsforsvg" class="svgBox">
+                    <object type="image/svg+xml" data="/images/icons/typescript.svg" width="34px" height="38px" ></object>
+                </div>
+                <div id="tsnamesvg" class="namesvg">TypeScript</div>
+             
+            </div>
+            <div id="tsprogressBar" class="bottomBox"></div>
+        </div>
+
+    </div>
+    <div id="col-7" class="col">
+        <div id="express-box" class="smallBox">
+            <div id="expresstopbox" class="topBox">
+                <div id="expressforsvg" class="svgBox">
+                    <object type="image/svg+xml" data="/images/icons/nodejs.svg" width="34px" height="38px" ></object>
+                </div>
+                <div id="expressnamesvg" class="namesvg">Express</div>
+             
+            </div>
+            <div id="expressprogressBar" class="bottomBox"></div>
+        </div>
+
+        <div id="vue-box" class="smallBox">
+            <div id="vuetopbox" class="topBox">
+                <div id="vueforsvg" class="svgBox">
+                    <object type="image/svg+xml" data="/images/icons/vue-js.svg" width="34px" height="38px" ></object>
+                </div>
+                <div id="vuenamesvg" class="namesvg">Vue.JS</div>
+             
+            </div>
+            <div id="vueprogressBar" class="bottomBox"></div>
+        </div>
+    </div>
+    <div id="col-8" class="col">
+        <div id="react-box" class="smallBox">
+            <div id="reacttopbox" class="topBox">
+                <div id="reactforsvg" class="svgBox">
+                    <object type="image/svg+xml" data="/images/icons/react-native.svg" width="34px" height="38px" ></object>
+                </div>
+                <div id="reactnamesvg" class="namesvg">React</div>
+             
+            </div>
+            <div id="reactprogressBar" class="bottomBox"></div>
+        </div>
+
+        <div id="vuex-box" class="smallBox">
+            <div id="vuextopbox" class="topBox">
+                <div id="vuexforsvg" class="svgBox">
+                    <object type="image/svg+xml" data="/images/icons/vue-js.svg" width="34px" height="38px" ></object>
+                </div>
+                <div id="vuexnamesvg" class="namesvg">VueX</div>
+             
+            </div>
+            <div id="vuexprogressBar" class="bottomBox"></div>
+        </div>
+    </div>
+    <div id="col-9" class="col">
+        <div id="redux-box" class="smallBox">
+            <div id="reduxtopbox" class="topBox">
+                <div id="reduxforsvg" class="svgBox">
+                    <object type="image/svg+xml" data="/images/icons/edux.svg" width="34px" height="38px" ></object>
+                </div>
+                <div id="reduxnamesvg" class="namesvg">Redux</div>
+             
+            </div>
+            <div id="reduxprogressBar" class="bottomBox"></div>
+        </div>
+    </div>
+</div>`;
+const preferredSoftware = 
+`<span class="header-software">Code editors:</span>
+<ul>
+    <li>Visual Studio Code</li>
+    <li>Sublime-text</li>
+</ul>
+<span class="header-software">Version control:</span>
+<ul>
+    <li>GitHub</li>
+    <li>GitBash</li>
+</ul>
+<span class="header-software">Database:</span>
+<ul>
+    <li>MySQL</li>
+    <li>HeidiSQL</li>
+</ul>
+<span class="header-software">Server side:</span>
+<ul>
+    <li>Node.JS</li>
+    <li>Express.JS</li>
+</ul>
+<span class="header-software">Testing API:</span>
+<ul>
+    <li>PostMan</li>
+</ul>
+<span class="header-software">Local server:</span>
+<ul>
+    <li>OpenServer</li>
+    <li>XAMPP</li>
+</ul>
+<span class="header-software">Virtualization:</span>
+<ul>
+    <li>VirtualBox</li>
+    <li>VMware</li>
+</ul>
+<span class="header-software">Browser:</span>
+<ul>
+    <li>Chrome</li>
+    <li>Responsively</li>
+</ul>
+<span class="header-software">Graphic editors:</span>
+<ul>
+    <li>Illustrator</li>
+    <li>Photoshop</li>
+</ul>
+<span class="header-software">Video editors:</span>
+<ul>
+    <li>Vegas</li>
+    <li>AfterEffect</li>
+</ul>
+<span class="header-software">Resourses:</span>
+<ul>
+    <li>developer.mozilla.org</li>
+    <li>w3school.com</li>
+    <li>dev.mysql.com</li>
+    <li>ru.vuejs.org</li>
+    <li>nodejs.org</li>
+    <li>stackoverflow.com</li>
+    <li>metanit.com</li>
+    <li>caniuse.com</li>
+    <li>color.adobe.com</li>
+</ul>`;
+const additionalKnowledge = 
+`<span class="header-additional">Automated Control Systems</span><br><br>
+<span class="header-software">PLC:</span>
+<ul>
+    <li>Siemens</li>
+    <li>Honeywell</li>
+    <li>Allen Bradley</li>
+    <li>GE Fanuc</li>
+</ul>
+<span class="header-software">Language:</span>
+<ul>
+    <li>STL</li>
+    <li>LAD</li>
+    <li>FBD</li>
+    <li>SFC</li>
+</ul>
+<span class="header-software">SCADA:</span>
+<ul>
+    <li>Tia Portal</li>
+    <li class="smallLi">Honeywell Process Solution</li>
+    <li>WinCC</li>
+    <li>Proficy</li>
+    <li>Rockwell Automation</li>
+</ul>`;
+const aboutMyself = 
+`<span class="header-software">Name / Age</span>
+<ul>
+    <li>Rasulov Timur / 31</li>
+</ul>
+<span class="header-software">E-mail:</span>
+<ul>
+    <li><a href="mailto:rasulov.lg@gmail.com">rasulov.lg@gmail.com</a></li>
+</ul>
+<span class="header-software">Phone:</span>
+<ul>
+    <li><a href="tel:+998335530650">+998(33)553-06-50</a></li>
+</ul>
+<span class="header-software">Telegram:</span>
+<ul>
+    <li><a href="tg://resolve?domain=@zombJkee">@zombJkee</a></li>
+</ul>
+<span class="header-software">Skype:</span>
+<ul>
+    <li><a href="skype:live:rasulov.lg?userinfo">live:rasulov.lg</a></li>
+</ul>
+<span class="header-software">Git:</span>
+<ul>
+    <li class="smallLi"><a href="https://github.com/zombjke">https://github.com/zombjke</a></li>
+</ul>
+<span class="header-software">Languages:</span>
+<ul>
+    <li>Russian - C2</li>
+    <li>English - A2/B1</li>
+</ul>
+<span class="header-software">Personal qualities:</span>
+<ul>
+    <li>Team player</li>
+    <li>Purposeful</li>
+    <li>Sociable</li>
+</ul>
+<span id="header-useful" class="header-software">Useful experience:</span>
+    <p>Development of a user interface for control systems of oil and gas plants</p><hr>
+    <p>Development and implementation of automated production control systems</p><hr>
+    <p>Development of an ERP system for a service center</p>
+`;
+const photo = 
+`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 506 506">
+<circle cx="253" cy="253" r="253"/>
+</svg>`;
+const mobileBody = 
+`<div id="title-back" class="back">
+<div id="back-col-mobile" class="back-col">
+<div id="title-road" class="title" onclick="createDetail_(4)">
+   <span id="roadmap" class="upper">roadmap</span><br>
+   <span id="ofknow" class="little">of knowledge</span>
+</div>
+<div id="title-additional" class="title" onclick="createDetail_(2)">
+   <span id="knowledge" class="little">knowledge</span><br>
+   <span id="additional" class="upper">additional</span>
+</div>
+<div id="title-software" class="title" onclick="createDetail_(1)">
+   <span id="preferred" class="little">preferred</span><br>
+   <span id="software" class="upper">software</span>
+</div>
+<div id="title-about" class="title" onclick="createDetail_(3)">
+   <span id="about" class="upper">about</span><br>
+   <span id="myself" class="little">myself</span>
+</div>
+
+
+</div>`;
+const pcBody = `<div id="title-back" class="back">
+<div id="for-photo" class="for-photo">
+    <svg xmlns="http://www.w3.org/2000/svg" width="506" height="506" viewBox="0 0 506 506">
+        <circle cx="253" cy="253" r="253"/>
+    </svg>
+</div>    
+<div id="back-col-1" class="back-col">
+    <div id="title-road" class="title" onclick="createRoadMap()">
+        <span id="roadmap" class="upper">roadmap</span><br>
+        <span id="ofknow" class="little">of knowledge</span>
+    </div>
+    <div id="title-additional" class="title" onclick="createDetail(2)">
+        <span id="knowledge" class="little">knowledge</span><br>
+        <span id="additional" class="upper">additional</span>
+    </div>
+</div>
+<div id="back-col-2" class="back-col">
+    <div id="title-about" class="title" onclick="createDetail(3)">
+        <span id="about" class="upper">about</span><br>
+        <span id="myself" class="little">myself</span>
+    </div>
+    <div id="title-software" class="title" onclick="createDetail(1)">
+        <span id="preferred" class="little">preferred</span><br>
+        <span id="software" class="upper">software</span>
+    </div>
+</div>
+<div id="back-col-3" class="back-col">
+    <div id="title-detail" class="title">
+        <span id="detail" class="upper">detail</span><br>
+        <div id="detailBody">
+            
+
+        </div>
+    </div>
+</div>
+</div>
+</div>`;
+const forPic = 
+`<div id="forPic">
+<img src="/images/roadmap_small.jpg" alt="roadmap" onclick="createRoadMap()">
+</div>
+<div id="toBig" onclick="createRoadMap()"><span>Click to zoom</span></div>`;
---
+++ javascripts/script.js
@@ -0,0 +1,156 @@
+window.onload = function(){
+   window.innerWidth >= 500 ? createBody('pc') : createBody('mobile')
+}    
+/**draw roadmap */
+function createRoadMap(){
+    if (!document.getElementById('roadMapId')){
+        let map = document.createElement('div');
+        map.id = "roadMapId";
+        map.innerHTML = roadmap;
+        document.getElementById('wrapper').append(map);
+        document.getElementById('title-back').style.filter = "blur(15px)";
+        document.addEventListener('click', function(event){cancelByClick(event, map)}, 2);
+    }
+}
+function createRoadMapMobile(){
+        let road = document.createElement('div');
+        road.id = "smallRoadMap";
+        road.innerHTML = forPic;
+        document.getElementById('title-road').append(road);
+       
+    
+}
+/**close roadmap by click outside the block */
+function cancelByClick(event, map){
+    let searchPlace = document.getElementById('backplace');
+    if (searchPlace){
+        let cordsPlace = searchPlace.getBoundingClientRect();
+        
+        let lessX = event.clientX < cordsPlace.x;
+        let lessY = event.clientY < cordsPlace.y;
+        let moreX = event.clientX > cordsPlace.x + cordsPlace.width;
+        let moreY = event.clientY > cordsPlace.y + cordsPlace.height;
+
+        if (lessX || lessY || moreX || moreY){
+            document.getElementById('title-back').style.filter = "";
+            map.remove();
+            document.removeEventListener('click', function(event){cancelByClick(event, map)}, 2);
+        }
+    }
+}
+/**filling detail*/
+function createDetail(what){
+    let detailBody = document.getElementById('detailBody');
+        switch (what){
+            case 1: detailBody.innerHTML = preferredSoftware; break;
+            case 2: detailBody.innerHTML = additionalKnowledge; break;
+            case 3: detailBody.innerHTML = aboutMyself; break;
+        }
+        detailBody.scrollTop = "";
+}
+
+function createDetail_(what){
+        switch (what){
+            case 1: selectMenu('title-software'); break;
+            case 2: selectMenu('title-additional'); break;
+            case 3: selectMenu('title-about'); break;
+            case 4: selectMenu('title-road'); break;
+        }
+}
+
+
+
+function selectMenu(id){
+    let selector = document.getElementById('selector');
+    
+    if (!selector){
+        createSelector(id);
+    }else if (selector && selector.parentElement.id == id){
+        selector.parentElement.innerHTML = selector.innerHTML;
+        removeClass();
+    } else {
+        selector.parentElement.innerHTML = selector.innerHTML;
+        removeClass(selector.parentElement);
+        createSelector(id);
+    }   
+
+}
+function createSelector(id){
+    let tmp = document.getElementById(id).innerHTML;
+    document.getElementById(id).innerHTML = '';
+    let selector = document.createElement('div');
+    selector.id = "selector";
+    selector.className = "selector";
+    selector.innerHTML = tmp;
+    document.getElementById(id).append(selector);
+    changeClass(id);
+    fillDetail(id);
+}
+
+function fillDetail(id){
+    let detail = document.createElement('div');
+    detail.id = "detailBody";
+    detail.className = "detailBody";
+    document.getElementById(id).append(detail);
+    switch (id){
+        case 'title-road': createRoadMapMobile(); break;
+        case 'title-software': detail.innerHTML = preferredSoftware; break;
+        case 'title-additional': detail.innerHTML = additionalKnowledge; break;
+        case 'title-about': detail.innerHTML = aboutMyself; addPhoto(); break;
+    }
+}
+function addPhoto(){
+    let forPhoto = document.createElement('div');
+    forPhoto.id = "for-photo";
+    forPhoto.className= "for-photo";
+    forPhoto.innerHTML = photo;
+    document.getElementById('detailBody').prepend(forPhoto);
+}
+function changeClass(id){
+    document.getElementById(id).classList.add('title-open');
+    let titles = document.querySelectorAll('.title');
+    for(let a=0;a<titles.length;a++){
+        let spans = titles[a].querySelectorAll('span');
+        if (titles[a].id == id){
+            for(let i=0;i<spans.length;i++){
+                if(spans[i].classList.contains('upper')) spans[i].classList.add('upper-open');
+                if(spans[i].classList.contains('little')) spans[i].classList.remove('little-close'); spans[i].classList.add('little-open');
+            }
+        }else{
+            for(let i=0;i<spans.length;i++){
+                if(spans[i].classList.contains('little')) spans[i].classList.add('little-close');
+            }
+        }       
+    }
+}
+function removeClass(elem){
+    if (elem){
+        elem.classList.remove('title-open');
+        let spans = elem.querySelectorAll('span');
+        for(let i=0;i<spans.length;i++){
+            if(spans[i].classList.contains('upper-open')) spans[i].classList.remove('upper-open');
+            if(spans[i].classList.contains('little-open')) spans[i].classList.remove('little-open');
+            if(spans[i].classList.contains('little-close')) spans[i].classList.remove('little-close');
+        }
+    } else {
+        let titles = document.querySelectorAll('.title');
+        for(let i=0;i<titles.length;i++){
+            if (titles[i].classList.contains("title-open")) titles[i].classList.remove("title-open"); 
+            let spans = titles[i].querySelectorAll('span');
+            for(let i=0;i<spans.length;i++){
+                if(spans[i].classList.contains('upper-open')) spans[i].classList.remove('upper-open');
+                if(spans[i].classList.contains('little-open')) spans[i].classList.remove('little-open');
+                if(spans[i].classList.contains('little-close')) spans[i].classList.remove('little-close');
+            }
+        }
+    }
+}
+function createBody(device){
+    let wrap = document.getElementById('wrapper')
+        switch (device) {
+            case 'pc': wrap.innerHTML = pcBody; break;
+            case 'mobile': wrap.innerHTML = mobileBody; break;
+            default: wrap.innerHTML = pcBody; break;
+        }
+}
+
